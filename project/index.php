a4e77bcb59c88ee00a52ce78e1e61782a1fe81a8
Milo Adams <adamsl9@mymail.nku.edu>
2024-09-12 10:43:28 -0400

    Update index.php

---
+++ project/index.php
@@ -0,0 +1,15 @@
+<!-- TO DO -->
+<!-- Create an array to hold blog posts -->
+<!-- display all the titles and link them to details.php -->
+
+<?php
+$blog_array= [
+  [ // for each blog provide the following
+    'title'=>'',
+    'content'=>'',
+    'author'=>'',
+    'date'=>'',
+  ]
+]
+?>
+
