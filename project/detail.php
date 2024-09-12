5b6109218f761ee5273b8cee7a41bddfe30ca5c4
Milo Adams <adamsl9@mymail.nku.edu>
2024-09-12 11:31:29 -0400

    Update detail.php

--- project/detail.php
+++ project/detail.php
@@ -8,23 +8,24 @@
 $i=$_GET['index'];
 
 $blog_array= [
-  [ // for each blog provide the following
-    'title'=>'The best comfort foods',
-    'content'=>'blah blah blah',
-    'author'=>'Milo Adams',
-    'date'=>'09/12/2024',
-  ]
+    [ // for each blog provide the following
+        'title'=>'The best comfort foods',
+        'content'=>'blah blah blah',
+        'author'=>'Milo Adams',
+        'date'=>'09/12/2024',
+    ]
 ]
 ?>
 
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 <head>
-  
+
 </head>
 <body>
 <h1> <?=$blog_array[$i]['title']?> </h1>
-<h2> <?=$blog_array[$i]['author']?> </h2>
-  
+<h2> By: <?=$blog_array[$i]['author']?> </h2>
+<h3> By: <?=$blog_array[$i]['date']?> </h3>
+<p> <?=$blog_array[$i]['content']?> </p>
 </body>
 </html>
