190ee922e28082f972acadf558ad1de7ad671b4d
Milo Adams <adamsl9@mymail.nku.edu>
2024-09-12 11:31:46 -0400

    Update index.php

--- project/index.php
+++ project/index.php
@@ -1,26 +1,27 @@
 <!-- TO DO -->
 <!-- Create an array to hold blog posts -->
-<!-- display all the titles and link them to details.php -->
+<!-- Display only the blog selected from index -->
+<!-- Use Bootstrap to make it look nice -->
 
 <?php
 $blog_array= [
-  [ // for each blog provide the following
-    'title'=>'The best comfort foods',
-    'content'=>'blah blah blah',
-    'author'=>'Milo Adams',
-    'date'=>'09/12/2024',
-  ]
+    [ // for each blog provide the following
+        'title'=>'The best comfort foods',
+        'content'=>'blah blah blah',
+        'author'=>'Milo Adams',
+        'date'=>'09/12/2024',
+    ]
 ]
 ?>
 
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 <head>
-  
+
 </head>
 <body>
-    <?php for($i=0;$i<count($blog_array);$i++) { ?>
-  <h2><?=$blog_array[$i]['title']></h2>
+<?php for($i=0;$i<count($blog_array);$i++) { ?>
+<h2><?=$blog_array[$i]['title']?></h2>
   <p><a href="detail.php?index=<?= $i ?>">Go to blog</a></p>
    <? } ?>
 </body>
