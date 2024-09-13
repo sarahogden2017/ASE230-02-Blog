b5fff5afe9976c8595dc8aa82bf1dcac7d0a56c5
kaylasaun <146030335+kaylasaun@users.noreply.github.com>
2024-09-12 21:48:08 -0400

    Update detail.php

--- project/detail.php
+++ project/detail.php
@@ -16,8 +16,15 @@ $blog_array= [
     ],
     [
         'title'=>'Top 3 Video Games of 2024',
-		'author'=>'Kayla Saunders',
-		'date'=>'09/12/2024',
+	'content'=>'Here are the top 3 video games of 2024:<br>
+	<b>Astro Bot</b><br>
+	This game puts you in the body of a robot named Astro and has you explore a variety of planets to rescue Astro\'s lost robot friends. A creative game with amazing graphics! Astro Bot introduces new gameplay ideas and makes hundreds of references and homages to many other video games.<br>
+	<b>Star Wars Outlaws</b><br>
+	In this game, you play as Kay Vess, oung scoundrel with a price on her head looking for a big score. This gorgeous game relies on a combination of stealthy sneaking and blaster combat.<br>
+	<b>Price of Persia The Lost Crown</b><br>
+	This game is a reboot, but pulls from some of the best side-scrolling video games. You are put in the role of Sargon, an Immortal tasked with rescuing a prince. The controls are very intuitive and responds instantly.<br>',
+	'author'=>'Kayla Saunders',
+	'date'=>'09/12/2024',
     ],
 ]
 ?>
@@ -34,7 +41,7 @@ $blog_array= [
     <body>
         <h1> <?=$blog_array[$i]['title']?> </h1>
         <h2> By: <?=$blog_array[$i]['author']?> </h2>
-        <h3> By: <?=$blog_array[$i]['date']?> </h3>
+        <h3> Published: <?=$blog_array[$i]['date']?> </h3>
         <p> <?=$blog_array[$i]['content']?> </p>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
